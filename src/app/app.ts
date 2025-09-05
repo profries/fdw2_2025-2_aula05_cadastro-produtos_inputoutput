5d90b718d65cfdda5245a1b5805c1e5fc45ac313
Luis Henrique Ries <prof.ries@gmail.com>
2025-09-05 14:31:55 -0300

    ng-content para passar as tags para o card

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,6 +15,7 @@ export class App {
     {id: 2, nome:"Produto 2", preco: 200},
     {id: 3, nome:"Produto 3", preco: 300},
     {id: 4, nome:"Produto 4", preco: 400},
+    {id: 5, nome:"Produto 5", preco: 500},
   ];
 
   addProduto(produto:any) {
--- src/app/card-produto/card-produto.ts
+++ src/app/card-produto/card-produto.ts
@@ -7,5 +7,4 @@ import { Component, Input } from '@angular/core';
   styleUrl: './card-produto.css'
 })
 export class CardProduto {
-  @Input() produto: any = { id:1, nome:"Produto 1", preco: 100 };
 }
